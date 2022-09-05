1f2fbdb5bb8bc2e431e1f2413437172550fa69d9
collo <mutaic84@gmail.com>
2022-09-06 01:24:04 +0300

    initial detail component update

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,8 +1,33 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
+import { exerciseOptions, fetchData } from '../utils/fetchData'
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = React.useState({})
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    const fetchExercisesData = async() => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com"
+      const youtubeSearchUrl = "https://youtube-search-and-download.p.rapidapi.com"
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+    }
+
+    fetchExercisesData();
+
+  }, [id])
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
