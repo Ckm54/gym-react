c744a7d9b411b28f4812fbd9425acf4ac9391c35
collo <mutaic84@gmail.com>
2022-09-05 18:15:53 +0300

    initial layout

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
       <Navbar />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
