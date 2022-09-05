81c41019c71f462e405d661cc4a7ce8498e120ad
collo <mutaic84@gmail.com>
2022-09-06 00:46:52 +0300

    integrate pagination

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,10 +1,21 @@
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Pagination, Stack, Typography } from '@mui/material'
 import React from 'react'
 import ExerciseCard from './ExerciseCard'
 
 const Exercises = ({exercises, setExercises, bodyPart}) => {
 
-  const[] = React.useState([])
+  const[currentPage, setCurrentPage] = React.useState(1)
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({top: 1800, behavior: 'smooth'})
+  }
+
   return (
     <Box id="exercises" sx={{
       mt: {lg: '110px'}
@@ -12,9 +23,15 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
       <Typography variant='h3' mb='40px'>Showing Results</Typography>
 
       <Stack direction="row" sx={{gap: {lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent={'center'}>
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
+      </Stack>
+
+      <Stack mt="100px" alignItems={'center'}>
+          {exercises.length > exercisesPerPage && (
+            <Pagination color='standard' shape='rounded' defaultPage={1} count={Math.ceil(exercises.length / 9)} page={currentPage} onChange={paginate} size="large" />
+          )}
       </Stack>
     </Box>
   )
