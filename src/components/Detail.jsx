353f44ce4ea93a06f6df19397f09bdbcdd7282d4
collo <mutaic84@gmail.com>
2022-09-06 09:43:08 +0300

    add exercise details component

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
-import { Stack, Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 
 const Detail = ({exerciseDetail}) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart
+    },
+    {
+      icon: TargetImage,
+      name: target
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment
+    },
+  ]
   return (
     <Stack gap="60px" sx={{
       flexDirection: {lg: 'row'}, p: '20px', alignItems: 'center'
@@ -18,6 +33,14 @@ const Detail = ({exerciseDetail}) => {
           It will help you improve your mood and gain energy.
         </Typography>
         <Typography variant='h6'>{name}</Typography>
+        {extraDetail.map((item) => (
+          <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+            <Button sx={{background: '#fff2db', borderRadius: '50%', width: '100px', height: '100px'}}>
+              <img src={item.icon} alt={bodyPart} style={{width: '50px', height: '50px'}} />
+            </Button>
+            <Typography variant="h5" textTransform={'capitalize'}>{item.name}</Typography>
+          </Stack>
+        ))}
       </Stack>
     </Stack>
   )
